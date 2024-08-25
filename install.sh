61803451f6d78f19a94edd10e7901b21bbfcd680
ahmadtoranji <98624883+ahmadtoranji@users.noreply.github.com>
2024-08-25 20:27:55 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,21 +13,26 @@ sudo apt-get upgrade -y
 echo "Installing Docker..."
 # Remove old Docker versions if they exist
 sudo apt-get remove docker docker-engine docker.io containerd runc -y
+
 # Install dependencies
 sudo apt-get install \
     ca-certificates \
     curl \
     gnupg \
     lsb-release -y
+
 # Add Docker's official GPG key
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
 # Add Docker repository
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 # Update package index and install Docker
 sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
 # Enable and start Docker service
 sudo systemctl enable docker
 sudo systemctl start docker
